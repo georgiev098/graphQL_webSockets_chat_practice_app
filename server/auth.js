5dd24ef5904150ac2760f93b011215ac9f21ffcc
geor0014 <geor0014@hz.nl>
2024-12-24 11:05:17 +0200

    finish subscription project

--- server/auth.js
+++ server/auth.js
@@ -1,11 +1,11 @@
-import { expressjwt } from 'express-jwt';
-import jwt from 'jsonwebtoken';
-import { getUser } from './db/users.js';
+import { expressjwt } from "express-jwt";
+import jwt from "jsonwebtoken";
+import { getUser } from "./db/users.js";
 
-const secret = Buffer.from('+Z3zPGXY7v/0MoMm1p8QuHDGGVrhELGd', 'base64');
+const secret = Buffer.from("+Z3zPGXY7v/0MoMm1p8QuHDGGVrhELGd", "base64");
 
 export const authMiddleware = expressjwt({
-  algorithms: ['HS256'],
+  algorithms: ["HS256"],
   credentialsRequired: false,
   secret,
 });
@@ -18,6 +18,10 @@ export async function handleLogin(req, res) {
   } else {
     const claims = { sub: username };
     const token = jwt.sign(claims, secret);
-    res.json({ token });  
+    res.json({ token });
   }
+}
+
+export function decodeToken(token) {
+  return jwt.verify(token, secret);
 }
